e9f7a7dd1037188c5052048a1b8a7205d1da658b
Shinku <ink39x@gmail.com>
2021-12-26 20:52:44 +0800

    fix: config tailwindcss/nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
+    require("postcss-import"),
+    require("tailwindcss/nesting"),
     require("tailwindcss"),
-    require("postcss-nested"), // or require('postcss-nesting')
     require("autoprefixer"),
   ],
 }
