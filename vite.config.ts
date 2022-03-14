50caed399fb2afd7b067fa89fa96745c856e5702
Shinku <ink39x@gmail.com>
2022-03-14 19:59:28 +0800

    fix: network host config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import svgr from "@honkhonk/vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    host: "0.0.0.0",
+  },
 })
